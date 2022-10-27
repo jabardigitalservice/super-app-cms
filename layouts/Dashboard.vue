756094b12b5e69fb52e1061fcb8481a5d4f44242
Yoslie <yoslie.tan@gmail.com>
2022-10-27 10:30:30 +0700

    refactor: remove component sidebar

--- layouts/Dashboard.vue
+++ layouts/Dashboard.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="bg-[#EDEFF3]">
-    <Sidebar class="bg-white w-min-[260px] fixed left-0 top-0 h-screen" />
     <div class="ml-[260px] px-6 py-[26px] h-max">
       <Nuxt />
     </div>
