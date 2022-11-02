5c7a918009652b9c0d629fe18659cc79bf491c74
Yoslie <yoslie.tan@gmail.com>
2022-11-02 14:52:40 +0700

    feat: slicing layout modal detail address

---
+++ components/KlaimRW/AddressTable/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="rounded-lg overflow-x-scroll font-roboto table-content">
+    <jds-simple-table>
+      <thead class="border">
+        <tr>
+          <th>Kota/Kabupaten</th>
+          <th>Kecamatan</th>
+          <th>Kelurahan/Desa</th>
+          <th>Dusun</th>
+          <th>RT/RW</th>
+          <th>Alamat Lengkap</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="text-gray-800">
+          <td>Data</td>
+          <td>Data</td>
+          <td>Data</td>
+          <td>Data</td>
+          <td>Data</td>
+          <td>Data</td>
+        </tr>
+      </tbody>
+    </jds-simple-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentAddressTable',
+  props: {
+    /**
+     * data table
+     */
+    dataTable: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
+
+<style>
+  .jds-simple-table {
+    border-spacing: 1px !important;
+    @apply !bg-[#EDEFF3];
+  }
+
+  .jds-simple-table tr th {
+    @apply min-w-[189px] border-r border-white;
+  }
+
+  .jds-simple-table tr td {
+    @apply min-w-[189px] border-r border-gray-200;
+  }
+
+  .table-content{
+    scrollbar-color: #E0E0E0 transparent;
+    scrollbar-width: thin;
+    scrollbar-track-color: #FAFAFA;
+  }
+
+  .table-content::-webkit-scrollbar{
+    @apply w-5 h-5;
+  }
+
+  .table-content::-webkit-scrollbar-track {
+    @apply bg-gray-50 rounded-b-lg;
+  }
+
+  .table-content::-webkit-scrollbar-thumb {
+    @apply bg-gray-300 rounded-xl border-solid border-[6px] border-transparent bg-clip-content;
+  }
+
+</style>
---
+++ components/KlaimRW/DetailAddress/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <transition name="fade">
+    <div v-if="show" class="fixed h-screen w-screen bg-black bg-opacity-75 z-100 inset-0 flex justify-center items-center">
+      <div class="w-[600px] min-h-min rounded-lg bg-white flex flex-col gap-4">
+        <h6 class="text-green-700 font-roboto font-medium text-[21px] px-4 pt-4">
+          {{ title }}
+        </h6>
+        <div class="max-h-[calc(100vh-64px-120px)] overflow-scroll flex flex-col gap-4 px-4 text-sm">
+          <div>
+            <table>
+              <tr class="text-gray-600">
+                <td>Nama RW</td>
+                <td><span class="ml-4">Email</span></td>
+              </tr>
+              <tr class="text-gray-800">
+                <td>Asep Kumaha</td>
+                <td><span class="ml-4">asepkumaha@gmail.com</span></td>
+              </tr>
+            </table>
+          </div>
+          <div>
+            <div class="mb-2 text-gray-800 font-bold">
+              Alamat Sesuai KTP
+            </div>
+            <KlaimRWAddressTable />
+          </div>
+          <div>
+            <div class="mb-2 text-gray-800 font-bold">
+              Alamat Sesuai Domisili
+            </div>
+            <KlaimRWAddressTable />
+          </div>
+        </div>
+        <div class="flex justify-center p-4 bg-gray-50 px-6 rounded-lg">
+          <jds-button label="Oke" variant="primary" @click="onClose" />
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'DetailAddress',
+  props: {
+    /**
+     * make modal visible or not
+     */
+    show: {
+      type: Boolean,
+      default: false
+    },
+    /**
+     * title modal
+     */
+    title: {
+      type: String,
+      default: ''
+    },
+    /**
+     * data rw
+     */
+    data: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  methods: {
+    onClose () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
