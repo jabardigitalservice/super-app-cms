f6188884fcd075dfba66dd2ae61f67cd29749fb2
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-11-08 17:19:08 +0700

    feat : update popup logout confirmation

---
+++ components/Logout/LogoutConfirmation.vue
@@ -0,0 +1,28 @@
+<template>
+  <BaseDialog
+    title="Keluar Web Admin"
+    confirmation-type="logout"
+    dialog-type="confirmation"
+    :show-popup="showPopup"
+    label-button="Ya, saya yakin"
+    @submit="$emit('logout')"
+    @close="$emit('close')"
+  >
+    <p class="text-[14px] text-gray-800 text-center py-[24px]">
+      Apakah Anda yakin akan keluar dari Web Admin Sapawarga?
+    </p>
+  </BaseDialog>
+</template>
+
+<script>
+
+export default {
+  name: 'LogoutConfirmation',
+  props: {
+    showPopup: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
