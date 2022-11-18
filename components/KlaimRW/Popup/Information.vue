db991c82d3898f84e8e3a950b754ce8ea4d3701e
Yoslie <yoslie.tan@gmail.com>
2022-11-18 09:40:59 +0700

    update: add conditional show message and icon on popup information klaim rw

--- components/KlaimRW/Popup/Information.vue
+++ components/KlaimRW/Popup/Information.vue
@@ -8,7 +8,8 @@
   >
     <div class="py-[16px] font-lato text-gray-800">
       <div class="flex justify-start items-start">
-        <jds-icon name="check-mark-circle" class="w-[16px] h-[16px] mt-1" fill="#069550" />
+        <jds-icon v-if="message" name="check-mark-circle" class="w-[16px] h-[16px] mt-1 text-green-700" />
+        <jds-icon v-else name="times-circle" class="w-[16px] h-[16px] mt-1 text-red-600" />
         <div class="ml-[13px]">
           <p class="text-[14px] text-gray-800 font-lato">
             {{ descriptionText }}
@@ -18,7 +19,7 @@
           </div>
         </div>
       </div>
-      <div class="bg-blue-50 border border-blue-500 h-fit w-full py-[8px] pr-[12px] pl-[14px] rounded-lg mt-[24px]">
+      <div v-if="message" class="bg-blue-50 border border-blue-500 h-fit w-full py-[8px] pr-[12px] pl-[14px] rounded-lg mt-[24px]">
         <div class="flex">
           <jds-icon name="exclamation-mark-circle" class="w-[16px] h-[16px] mt-1 mr-[18px]" fill="#1565C0" />
           <div class="text-[14px] font-lato">
