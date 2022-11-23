9a4b72758445defaa75a327eb6f157670448408b
Yoslie <yoslie.tan@gmail.com>
2022-11-23 09:50:04 +0700

    style: add padding to no data view

--- components/KlaimRW/ViewDocument/index.vue
+++ components/KlaimRW/ViewDocument/index.vue
@@ -1,7 +1,7 @@
 <template>
   <transition name="fade">
     <div v-if="show" class="fixed h-screen w-screen bg-black bg-opacity-75 z-50 inset-0 flex justify-center items-center">
-      <div class="w-[600px] min-h-min rounded-lg bg-white flex flex-col gap-4">
+      <div class="w-[700px] min-h-min rounded-lg bg-white flex flex-col gap-4">
         <h1 class="text-green-700 font-roboto font-medium text-[21px] px-4 pt-4">
           Dokumen SK RW
         </h1>
@@ -12,7 +12,7 @@
             class="min-h-[600px] w-full"
           >
         </div>
-        <div v-else>
+        <div v-else class="px-4">
           Tidak ada data
         </div>
         <div class="flex justify-center p-4 bg-gray-50 px-6 rounded-lg">
