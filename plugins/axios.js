2297024e671cb9e947214f65045b89a9a7b6e70d
Adzhar Amrullah <adzharamrullah@gmail.com>
2022-12-07 14:38:48 +0700

    chore: add x-timestamp on header request

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,11 @@
+import { formatInTimeZone } from 'date-fns-tz'
+
 export default function ({ $axios, $config }) {
   $axios.onRequest((config) => {
     const url = config.url
     if (!url.includes($config.keyclockBaseUrl)) {
       $axios.setHeader('Api-Key', $config.apiKey)
+      $axios.setHeader('X-Timestamp', formatInTimeZone(new Date(), 'Asia/Jakarta', "yyyy-MM-dd'T'HH:mm:ssXXX"))
     }
 
     return config
