d1d2b44883f45617a13e569ed197383d1c3d7234
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-01-03 14:30:44 +0700

    chore: remove  google analytics package

--- plugins/vue-gtag.js
+++ plugins/vue-gtag.js
@@ -2,14 +2,7 @@ import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
 export default function ({ $config, app }) {
-/*
-    try using vue-gtag
-    https://stackoverflow.com/questions/64612031/setup-google-analytics-4-in-nuxt-js/64613134#64613134
-    auto tracking page
-    https://github.com/MatteoGabriele/vue-gtag/issues/249
-    I try using Universal Analytics(code:UA-XXXXX-X), but the tracking page in google analytics was updated.
-*/
   Vue.use(VueGtag, {
-    config: { id: $config.googleAnalytics.id }
+    config: { id: $config.googleAnalyticsId }
   }, app.router)
 }
