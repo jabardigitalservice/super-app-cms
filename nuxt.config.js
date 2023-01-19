4042f77edc78deab36e28b5175ae72e8bd57e318
Ray Fajar S <rayfajars.com>
2023-01-19 14:01:15 +0700

    chore: change favicon in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
 
@@ -50,6 +50,8 @@ export default {
     // auth
     '@nuxtjs/auth-next'
   ],
+
+  devModules: ['@nuxtjs/eslint-module'],
 
   auth: {
     redirect: {
