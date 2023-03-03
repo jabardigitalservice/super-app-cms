db6b2717df8d23f4035b4acde6fa651aea8b0ad6
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-03-03 15:10:17 +0700

    refactor: add error message in edit status popup claim RW

--- components/KlaimRW/Popup/EditStatus.vue
+++ components/KlaimRW/Popup/EditStatus.vue
@@ -4,7 +4,7 @@
     :show-popup="showPopup"
     label-button="Simpan"
     dialog-type="confirmation"
-    @submit="$emit('submit',statusRw)"
+    @submit="editStatusHandle"
     @close="closeHandle"
   >
     <div class="mt-6">
@@ -14,8 +14,15 @@
       <p class="text-[16px] font-lato text-gray-800 font-bold">
         <strong>{{ accountName }}</strong>
       </p>
-      <form class="py-4 select__form">
-        <jds-select placeholder="Pilih status" label="Status Akun RW" :options="statusOptions" :value="getValueStatusRw" @change="getStatusFromSelect" />
+      <form class="py-4 select__form" :class="{'mb-4':errorMessage!==''}">
+        <jds-select
+          placeholder="Pilih status"
+          label="Status Akun RW"
+          :options="statusOptions"
+          :value="getValueStatusRw"
+          :error-message="errorMessage"
+          @change="getStatusFromSelect"
+        />
       </form>
     </div>
   </BaseDialog>
@@ -52,7 +59,8 @@ export default {
       statusRw: '',
       userStatus,
       // isChangeStatus is used for check if status claim rw change from select form
-      isChangeStatus: false
+      isChangeStatus: false,
+      errorMessage: ''
     }
   },
   computed: {
@@ -69,8 +77,23 @@ export default {
       this.statusRw = value
       this.isChangeStatus = true
     },
+    validateSelectFormHandle () {
+      let result = ''
+      if (this.statusRw === '' || this.statusRw === userStatus.waiting) {
+        result = 'Status Akun RW wajib diisi.'
+      }
+      return result
+    },
+    editStatusHandle () {
+      if (this.validateSelectFormHandle() === '') {
+        this.$emit('submit', this.statusRw)
+      } else {
+        this.errorMessage = this.validateSelectFormHandle()
+      }
+    },
     closeHandle () {
       this.$emit('close')
+      this.errorMessage = ''
       this.statusRw = this.accountStatus
     }
   }
