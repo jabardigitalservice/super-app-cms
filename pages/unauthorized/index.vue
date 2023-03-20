5c9f1516d3cd1c344741f6c7e5815f516088ead0
Ray Fajar S <rayfajars.com>
2023-03-20 16:01:09 +0700

    feat: add pages unauthorized

---
+++ pages/unauthorized/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <Maintenance :title="title" />
+</template>
+
+<script>
+export default {
+  name: 'PageUnauthorized',
+  layout: 'Dashboard',
+  data () {
+    return {
+      title: 'Oops, Anda tidak memiliki akses pada fitur ini!'
+    }
+  },
+  mounted () {
+    this.$store.commit('setActivePage', '')
+    this.$store.commit('setHeader', {
+      navigations: [],
+      descriptionPage: ''
+    })
+  }
+}
+</script>
