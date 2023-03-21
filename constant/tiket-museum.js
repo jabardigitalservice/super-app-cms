f329ae823b129ff15867830e70673d36d12b5e20
Ray Fajar S <rayfajars.com>
2023-03-21 14:21:50 +0700

    feat: add value confirmation

--- constant/tiket-museum.js
+++ constant/tiket-museum.js
@@ -55,3 +55,79 @@ export const ticketStatus = {
   expired: 'Kadaluarsa',
   ordered: 'Dipesan'
 }
+
+export const rejectConfirmationPopup = {
+  title: 'Konfirmasi Reject',
+  descriptionText: 'Apakah Anda yakin ingin melakukan reject pada tiket ini?',
+  dialogType: 'confirmation',
+  buttonLeft: {
+    label: 'Batal',
+    variant: 'secondary'
+  },
+  buttonRight: {
+    label: 'Reject Tiket',
+    variant: 'danger'
+  }
+}
+
+export const rejectInformationPopup = {
+  title: 'Reject Tiket',
+  successInformation: {
+    descriptionText: 'Tiket ',
+    dialogType: 'information',
+    buttonRight: {
+      label: 'Saya mengerti',
+      variant: 'primary'
+    }
+  },
+  failedInformation: {
+    descriptionText: 'Tiket gagal direject',
+    dialogType: 'confirmation',
+    buttonRight: {
+      label: 'Coba Lagi',
+      variant: 'primary'
+    }
+  },
+  buttonLeft: {
+    label: 'Batal',
+    variant: 'secondary'
+  }
+}
+
+export const verificationConfirmationPopup = {
+  title: 'Konfirmasi Verifikasi',
+  descriptionText: 'Apakah Anda yakin ingin memverifikasi tiket ini?',
+  dialogType: 'confirmation',
+  buttonLeft: {
+    label: 'Batal',
+    variant: 'secondary'
+  },
+  buttonRight: {
+    label: 'Ya Verifikasikan',
+    variant: 'primary'
+  }
+}
+
+export const verificationInformationPopup = {
+  title: 'Verifikasi Tiket',
+  successInformation: {
+    descriptionText: 'Tiket berhasil diverifikasi',
+    dialogType: 'information',
+    buttonRight: {
+      label: 'Saya mengerti',
+      variant: 'primary'
+    }
+  },
+  failedInformation: {
+    descriptionText: 'Tiket gagal diverifikasi',
+    dialogType: 'confirmation',
+    buttonRight: {
+      label: 'Coba Lagi',
+      variant: 'primary'
+    }
+  },
+  buttonLeft: {
+    label: 'Batal',
+    variant: 'secondary'
+  }
+}
