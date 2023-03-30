94d66b468bd50c768d4e2514491320a8cc4e32ee
Ray Fajar S <rayfajars.com>
2023-03-30 15:02:32 +0700

    refactor: code plugins get rolse

--- plugins/get-roles.js
+++ plugins/get-roles.js
@@ -6,10 +6,6 @@ export default ({ $auth }, inject) => {
     const token = tokenBearer.split(' ')
     const tokenDecode = VueJwtDecode.decode(token[1])
 
-    if (tokenDecode.realm_access.roles.includes('admin')) {
-      inject('role', 'adminRW')
-    } else {
-      inject('role', 'adminTicket')
-    }
+    inject('role', tokenDecode.realm_access.roles)
   }
 }
