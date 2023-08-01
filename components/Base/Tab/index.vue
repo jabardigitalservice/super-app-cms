350ec9ebbe41f85f85f8dc301bf30c751e3b3355
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-08-01 09:11:36 +0700

    refactor : remove conditional if on base tab component

--- components/Base/Tab/index.vue
+++ components/Base/Tab/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-[70px] bg-green-700 py-2 px-3 min-w-[180px] rounded-t-lg" :class="{'!bg-white':selected}">
     <slot>
-      <button v-if="title!==''" class="text-sm text-green-100 items-center" :class="{'!text-gray-700':selected}">
+      <button class="text-sm text-green-100 items-center" :class="{'!text-gray-700':selected}">
         {{ title }}
       </button>
     </slot>
