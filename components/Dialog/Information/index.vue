f11a9dae276714d58724a374fd2f3107db4991d5
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-09-06 16:10:22 +0700

    feat : add dialog information, confirmation. loading & input text area on component dialog

---
+++ components/Dialog/Information/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <BaseDialog :show-popup="showPopup">
+    <BaseDialogPanel>
+      <BaseDialogHeader :title="dataDialog.title" />
+      <div class="flex items-start px-6 pt-4 pb-10">
+        <jds-icon :name="iconPopup.name" size="sm" :fill="iconPopup.fill" class="mr-3 mt-1" />
+        <BaseDialogDescription :description="dataDialog.description" :sub-description="dataDialog.subDescription" />
+      </div>
+      <BaseDialogFooter :show-cancel-button="dataDialog.showCancelButton" :label-button="dataDialog.labelButton" @close="$emit('close')" @submit="$emit('submit',dataDialog)" />
+    </BaseDialogPanel>
+  </BaseDialog>
+</template>
+
+<script>
+export default {
+  name: 'DialogInformation',
+  props: {
+    showPopup: {
+      type: Boolean,
+      default: false
+    },
+    dataDialog: {
+      type: Object,
+      default: () => {}
+    },
+    iconPopup: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
