7e5029b737de31c2939412fa33caec4764724ef5
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-09-27 13:45:39 +0700

    feat : slicing layout popup tracking status

---
+++ components/Aduan/Dialog/DetailStatusSpanLapor/CardSpanLapor/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="border border-b-0 border-x-gray-300 rounded-t-lg" :class="{'border !border-b border-b-gray-300 rounded-b-lg':isLastIndex}">
+    <div class="bg-[#F4F6F6] h-11 rounded-t-lg flex items-center p-3">
+      <jds-icon name="calendar-date-outline" size="sm" fill="#9E9E9E" />
+      <p class="text-gray-800 text-xs font-roboto ml-2">
+        {{ dataSpanLapor.dateSpanLapor }}
+      </p>
+    </div>
+    <div class="p-3">
+      <h1 class="text-sm text-gray-900 font-medium">
+        {{ dataSpanLapor.deliverSpanLapor }}
+      </h1>
+      <h2 class="text-gray-600 mt-2 text-sm">
+        Keterangan
+      </h2>
+      <p class="text-[14px] leading-[23px] text-gray-800 mt-1">
+        {{ dataSpanLapor.notes }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardSpanLapor',
+  props: {
+    dataSpanLapor: {
+      type: Object,
+      default: () => ({})
+    },
+    isLastIndex: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
---
+++ components/Aduan/Dialog/DetailStatusSpanLapor/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <BaseDialog :show-popup="showPopup">
+    <BaseDialogPanel class="w-[600px]">
+      <BaseDialogHeader title="Status Aduan SP4N Lapor" />
+      <div class="pl-6 pr-4">
+        <BaseDialogDescription
+          description="No.Aduan"
+          :sub-description="dataDialog.subDescription"
+          class="mt-2 pb-3"
+        />
+        <div class="h-[412px] overflow-y-auto dialog-content mb-[25px]">
+          <CardSpanLapor v-for="(itemSpanLapor,index) in listTrackingSpanLapor" :key="index" :data-span-lapor="itemSpanLapor" :is-last-index="index===listTrackingSpanLapor.length-1" />
+        </div>
+      </div>
+      <BaseDialogFooter label-button="Tutup" :show-cancel-button="false" @close="$emit('close')" />
+    </BaseDialogPanel>
+  </BaseDialog>
+</template>
+
+<script>
+import CardSpanLapor from '~/components/Aduan/Dialog/DetailStatusSpanLapor/CardSpanLapor'
+export default {
+  name: 'DialogDetailStatusSpanLapor',
+  components: { CardSpanLapor },
+  props: {
+    showPopup: {
+      type: Boolean,
+      default: true
+    },
+    listTrackingSpanLapor: {
+      type: Array,
+      default: () => ([])
+    },
+    dataDialog: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
+
+<style scoped>
+.dialog-content::-webkit-scrollbar {
+  @apply h-5 w-5;
+}
+
+.dialog-content::-webkit-scrollbar-track {
+  @apply bg-transparent;
+}
+
+.dialog-content::-webkit-scrollbar-thumb {
+  @apply rounded-xl border-[6px] border-solid border-transparent bg-gray-300 bg-clip-content;
+}
+</style>
