afd2e46ad1543b0d160ef890e3b8f141d5f284ce
Ray Fajar Salinggih <rayfajars@gmail.com>
2023-09-29 17:08:27 +0700

    feat: add pages preview pdf laporan pendapatan

---
+++ pages/tahura/preview-pdf/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <BasePreviewPdf />
+</template>
+
+<script>
+export default {
+  name: 'PreviewPdfTahura',
+  layout: 'Dashboard',
+  data () {
+    return {
+      navigations: [
+        {
+          label: 'Preview Laporan Pendapatan',
+          link: '/tahura/preview-pdf'
+        }
+      ],
+      descriptionPage: 'Preview Laporan Pendapatan Taman Hutan Raya.'
+    }
+  },
+  mounted () {
+    this.$store.commit('setActivePage', 'Laporan Pendapatan')
+    this.$store.commit('setHeader', {
+      navigations: this.navigations,
+      descriptionPage: this.descriptionPage
+    })
+  }
+}
+</script>
+
+  <style></style>
