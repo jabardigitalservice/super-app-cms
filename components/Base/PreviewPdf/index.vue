bd00e89efef28a6d3faf60a6f2ee539fed885559
Ray Fajar Salinggih <rayfajars@gmail.com>
2023-09-29 17:08:02 +0700

    feat: add component layouting preview pdf

---
+++ components/Base/PreviewPdf/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="bg-gray-100 font-serif">
+    <div class="w-full rounded-lg bg-white p-6 shadow-md">
+      <div class="mb-6">
+        <h1 class="text-3xl font-bold">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus molestias facere voluptate reprehenderit assumenda necessitatibus, deleniti quidem nobis harum obcaecati.
+        </h1>
+        <p class="text-gray-600">
+          Tanggal: September 29, 2023
+        </p>
+      </div>
+
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id
+          facilisis tellus, ac convallis eros.
+        </p>
+        <p>
+          Nullam sit amet tincidunt elit, id congue nunc. Phasellus in arcu nec
+          lectus gravida ullamcorper.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit
+          asperiores laborum doloremque vitae officiis et, fugit sit ducimus
+          quia facilis reiciendis, nemo voluptatibus, pariatur eum rem quaerat
+          optio ipsa. Officia fuga repudiandae odit, at incidunt sunt temporibus
+          harum quae facilis quas explicabo doloremque optio, reiciendis commodi
+          suscipit ex fugiat necessitatibus.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BasePreviewPdf'
+}
+</script>
+
+<style></style>
