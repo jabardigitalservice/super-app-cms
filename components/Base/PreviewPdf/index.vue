41b6ad2dae0c4dd3248a5c7f68a322f48db06d4d
Ray Fajar Salinggih <rayfajars@gmail.com>
2023-10-02 15:57:13 +0700

    feat: add component preview PDF and preview pdf laporan pendapatan tahura

--- components/Base/PreviewPdf/index.vue
+++ components/Base/PreviewPdf/index.vue
@@ -1,32 +1,15 @@
 <template>
   <div class="bg-gray-100 font-serif">
     <div class="w-full rounded-lg bg-white p-6 shadow-md">
-      <div class="mb-6">
-        <h1 class="text-3xl font-bold">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus molestias facere voluptate reprehenderit assumenda necessitatibus, deleniti quidem nobis harum obcaecati.
-        </h1>
-        <p class="text-gray-600">
-          Tanggal: September 29, 2023
-        </p>
-      </div>
+      <div class="container">
+        <div class="mb-6">
+          <h1 class="text-3xl font-bold">
+            <slot name="title" />
+          </h1>
+          <slot name="header" />
+        </div>
 
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id
-          facilisis tellus, ac convallis eros.
-        </p>
-        <p>
-          Nullam sit amet tincidunt elit, id congue nunc. Phasellus in arcu nec
-          lectus gravida ullamcorper.
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit
-          asperiores laborum doloremque vitae officiis et, fugit sit ducimus
-          quia facilis reiciendis, nemo voluptatibus, pariatur eum rem quaerat
-          optio ipsa. Officia fuga repudiandae odit, at incidunt sunt temporibus
-          harum quae facilis quas explicabo doloremque optio, reiciendis commodi
-          suscipit ex fugiat necessitatibus.
-        </p>
+        <slot name="body" />
       </div>
     </div>
   </div>
@@ -34,8 +17,7 @@
 
 <script>
 export default {
-  name: 'BasePreviewPdf'
-}
+  /*eslint-disable*/
+  name: "BasePreviewPdf",
+};
 </script>
-
-<style></style>
