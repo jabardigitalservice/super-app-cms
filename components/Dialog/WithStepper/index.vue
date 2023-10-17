bf9313706f9c440d0d72ddd3f06c18884c197252
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-10-17 07:26:46 +0700

    feat : add component dialog with stepper

---
+++ components/Dialog/WithStepper/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <BaseDialog :show-popup="showPopup">
+    <BaseDialogPanel class="w-[640px]">
+      <BaseDialogHeader :title="title" />
+      <ProgressStepper class="pt-4 px-6" :index-current-active="indexCurrentActive" />
+      <slot name="form-complaint" />
+      <slot name="footer" />
+    </BaseDialogPanel>
+  </BaseDialog>
+</template>
+
+<script>
+import ProgressStepper from '~/components/Dialog/WithStepper/ProgressStepper'
+export default {
+  name: 'DialogWithStepper',
+  components: { ProgressStepper },
+  props: {
+    showPopup: {
+      type: Boolean,
+      default: false
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    indexCurrentActive: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
