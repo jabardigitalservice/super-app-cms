dfba7f6a8484f1a899e5b93ded2bb8606d6f8bd7
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-10-18 13:52:33 +0700

    feat : add component footer on component dialog with stepper

--- components/Dialog/WithStepper/index.vue
+++ components/Dialog/WithStepper/index.vue
@@ -4,13 +4,26 @@
       <BaseDialogHeader :title="title" />
       <ProgressStepper class="pt-4 px-6" :index-current-active="indexCurrentActive" />
       <slot name="form-complaint" />
-      <slot name="footer" />
-    </BaseDialogPanel>
-  </BaseDialog>
+      <BaseDialogFooter>
+        <template #default>
+          <div class="flex justify-between">
+            <jds-button label="Batal" variant="tertiary" class="!font-bold !text-green-700" @click="$emit('close')" />
+            <div class="flex">
+              <div class="mr-3">
+                <jds-button label="Kembali" variant="secondary" class="!font-bold !border-green-700 !text-green-700" @click="$emit('prev')" />
+              </div>
+              <jds-button :label="labelButton" variant="primary" @click="$emit('submit')" />
+            </div>
+          </div>
+        </template>
+      </BaseDialogFooter>
+    </basedialogpanel>
+  </basedialog>
 </template>
 
 <script>
 import ProgressStepper from '~/components/Dialog/WithStepper/ProgressStepper'
+
 export default {
   name: 'DialogWithStepper',
   components: { ProgressStepper },
@@ -26,6 +39,15 @@ export default {
     indexCurrentActive: {
       type: Number,
       default: 0
+    },
+    labelButton: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    submitButtonHandle () {
+      this.$emit('submit')
     }
   }
 }
