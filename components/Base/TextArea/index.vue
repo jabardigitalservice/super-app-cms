e6fc556c39c10898ab58e8ecad8e2240b919781b
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-10-19 15:21:01 +0700

    refactor : remove some props atrribute ext area on component base text area

--- components/Base/TextArea/index.vue
+++ components/Base/TextArea/index.vue
@@ -4,8 +4,6 @@
     <div class="mt-1 input-wrapper h-[100px] bg-gray-50 rounded-lg border border-gray-400 py-2 px-[14px]" :class="{'mb-[2px] !border-red-600':errorMessage}">
       <textarea
         :value="value"
-        :name="name"
-        :placeholder="placeholder"
         class="h-full w-full bg-transparent resize-none font-lato placeholder:text-sm placeholder:text-gray-600 focus:outline-none"
 
         v-bind="$attrs"
@@ -33,22 +31,6 @@ export default {
       default: ''
     },
     errorMessage: {
-      type: String,
-      default: ''
-    },
-    placeholder: {
-      type: String,
-      default: ''
-    },
-    minLength: {
-      type: Number,
-      default: 0
-    },
-    maxLength: {
-      type: Number,
-      default: 200
-    },
-    name: {
       type: String,
       default: ''
     }
