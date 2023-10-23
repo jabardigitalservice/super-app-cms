981c9b9fc44b4e5515e38f66879e7f613841a4f0
rayfajars <rayfajars@gmail.com>
2023-10-23 14:51:39 +0700

    feat: integration API detail daftar pesanan

--- components/Tahura/DaftarPesanan/Detail/index.vue
+++ components/Tahura/DaftarPesanan/Detail/index.vue
@@ -23,75 +23,90 @@
           <BaseTableDetail header="Informasi Order" class="text-lato mb-4">
             <tr>
               <td class="w-[180px]">
+                <strong>Nama Pemesan</strong>
+              </td>
+              <td>{{ detailPesanan.orderBy }}</td>
+            </tr>
+            <tr>
+              <td class="w-[180px]">
                 <strong>No.Order</strong>
               </td>
-              <td>adsad</td>
+              <td>{{ detailPesanan.invoice }}</td>
             </tr>
             <tr>
               <td>
                 <strong>Jumlah Order</strong>
               </td>
-              <td>adsad</td>
+              <td>{{ detailPesanan.ticketCount }} Tiket</td>
             </tr>
             <tr>
               <td>
                 <strong>Tanggal Dipesan</strong>
               </td>
-              <td>adsad</td>
+              <td>{{ detailPesanan.orderedAt }} WIB</td>
             </tr>
             <tr>
               <td>
                 <strong>Tanggal Kunjungan</strong>
               </td>
-              <td>adsad</td>
+              <td>{{ detailPesanan.reservationDate }} WIB</td>
             </tr>
             <tr>
               <td>
                 <strong>Status Order</strong>
               </td>
-              <td>adsad</td>
+              <td>
+                <p
+                  v-show="detailPesanan?.statusCode"
+                  class="h-fit w-fit rounded-[32px] bg-gray-100 px-[10px] py-1 text-xs font-semibold"
+                  :class="statusTahura[detailPesanan.statusCode]?.color"
+                >
+                  {{ statusTahura[detailPesanan.statusCode]?.label }}
+                </p>
+              </td>
             </tr>
           </BaseTableDetail>
 
           <BaseTableDetail header="Informasi Pengunjung" class="text-lato mb-4">
-            <tr>
+            <tr v-for="(item, index) in detailPesanan.categories" :key="index">
               <td class="w-[180px]">
-                <strong>Total Wisatawan Lokal</strong>
+                <strong>{{ item.name }}</strong>
               </td>
-              <td>adsad</td>
-            </tr>
-            <tr>
-              <td>
-                <strong>Total Wisatawan Asing</strong>
-              </td>
-              <td>adsad</td>
+              <td>{{ item.quantity ? item.quantity : "-" }}</td>
             </tr>
           </BaseTableDetail>
 
-          <BaseTableDetail header="Informasi Scan" class="text-lato mb-4">
+          <BaseTableDetail
+            v-if="
+              detailPesanan?.statusCode === 'scanned' &&
+                detailScanned?.scannedStatus === 'success'
+            "
+            header="Informasi Scan"
+            class="text-lato mb-4"
+          >
             <tr>
               <td class="w-[180px]">
                 <strong>Discan oleh</strong>
               </td>
-              <td>adsad</td>
+              <td>{{ detailScanned?.scannedBy }}</td>
             </tr>
             <tr>
               <td>
                 <strong>Di Pintu</strong>
               </td>
-              <td>adsad</td>
+              <td>{{ detailScanned?.gateName }}</td>
             </tr>
             <tr>
               <td>
                 <strong>Tanggal Scan</strong>
               </td>
-              <td>adsad</td>
+              <td>{{ formatDate(detailScanned?.scannedAt, "eeee, dd MMMM yyyy") }}</td>
             </tr>
             <tr>
               <td>
                 <strong>Waktu Scan</strong>
               </td>
-              <td>adsad</td>
+              <td>{{ formatDate(detailScanned?.scannedAt, "HH:mm") }}</td>
             </tr>
           </BaseTableDetail>
         </BaseTabPanel>
@@ -103,9 +118,21 @@
 <script>
 import ArrowLeft from '~/assets/icon/arrow-left.svg?inline'
 import TabBarDetail from '~/components/Tahura/TabBar/Detail/index.vue'
+import { statusTahura } from '@/constant/tahura.js'
+import { formatDate } from '~/utils'
 export default {
   name: 'TahuraDaftarPesananDetail',
   components: { ArrowLeft, TabBarDetail },
+  props: {
+    detailPesanan: {
+      type: Object,
+      default: () => {}
+    },
+    detailScanned: {
+      type: Object,
+      default: () => {}
+    }
+  },
   data () {
     return {
       listTab: [
@@ -118,7 +145,9 @@ export default {
           icon: '/icon/qr-code.svg'
         }
       ],
-      selectedTabIndex: 0
+      selectedTabIndex: 0,
+      statusTahura,
+      dataScanned: {}
     }
   },
   methods: {
@@ -127,7 +156,8 @@ export default {
     },
     selectedTabHandle (index) {
       this.selectedTabIndex = index
-    }
+    },
+    formatDate
   }
 }
 </script>
