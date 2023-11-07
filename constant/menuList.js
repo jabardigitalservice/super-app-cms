8efb99bfd93de82a9a844641b066a5f28718bb4a
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-11-07 10:17:24 +0700

    refactor : remove comment menu list complaint

--- constant/menuList.js
+++ constant/menuList.js
@@ -81,41 +81,40 @@ const masterDataMenu = {
   ]
 }
 
-// TODO: comment code for production
-// const aduanMenu = {
-//   titleMenu: 'aduan',
-//   showTitleMenuForRoles: ['admin'],
-//   menu: [
-//     {
-//       name: 'Daftar Aduan Masuk',
-//       path: '/aduan/aduan-masuk',
-//       arrow: false,
-//       showMenuAndAccessForRoles: ['admin'],
-//       icon: 'clipboard-list.svg'
-//     },
-//     {
-//       name: 'Daftar Penentuan Kewenangan',
-//       path: '/aduan/penentuan-kewenangan',
-//       arrow: false,
-//       showMenuAndAccessForRoles: ['admin'],
-//       icon: 'clipboard-user.svg'
-//     },
-//     {
-//       name: 'Dialihkan ke SP4N',
-//       path: '/aduan/dialihkan-ke-span-lapor',
-//       arrow: false,
-//       showMenuAndAccessForRoles: ['admin'],
-//       icon: 'clipboard-arrow-left.svg'
-//     },
-//     {
-//       name: 'Daftar Aduan dari SP4N',
-//       path: '/aduan/aduan-dari-span-lapor',
-//       arrow: false,
-//       showMenuAndAccessForRoles: ['admin'],
-//       icon: 'clipboard-pencil.svg'
-//     }
-//   ]
-// }
+const aduanMenu = {
+  titleMenu: 'aduan',
+  showTitleMenuForRoles: ['admin'],
+  menu: [
+    {
+      name: 'Daftar Aduan Masuk',
+      path: '/aduan/aduan-masuk',
+      arrow: false,
+      showMenuAndAccessForRoles: ['admin'],
+      icon: 'clipboard-list.svg'
+    },
+    {
+      name: 'Daftar Penentuan Kewenangan',
+      path: '/aduan/penentuan-kewenangan',
+      arrow: false,
+      showMenuAndAccessForRoles: ['admin'],
+      icon: 'clipboard-user.svg'
+    },
+    {
+      name: 'Dialihkan ke SP4N',
+      path: '/aduan/dialihkan-ke-span-lapor',
+      arrow: false,
+      showMenuAndAccessForRoles: ['admin'],
+      icon: 'clipboard-arrow-left.svg'
+    },
+    {
+      name: 'Daftar Aduan dari SP4N',
+      path: '/aduan/aduan-dari-span-lapor',
+      arrow: false,
+      showMenuAndAccessForRoles: ['admin'],
+      icon: 'clipboard-pencil.svg'
+    }
+  ]
+}
 
 const tahuraMenu = {
   titleMenu: 'Ticketing Tahura',
@@ -156,7 +155,6 @@ export const menu = [
   messageNotifMenu,
   tiketMuseumMenu,
   masterDataMenu,
-  // TODO: comment code for production
-  // aduanMenu,
+  aduanMenu,
   tahuraMenu
 ]
