d86f07ba688570e048a2b803f1c38289ff101e83
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-11-14 15:53:27 +0700

    refactor: change info icon name on component alert message

--- components/Aduan/Dialog/ProcessComplaint/AlertMessage/index.vue
+++ components/Aduan/Dialog/ProcessComplaint/AlertMessage/index.vue
@@ -3,7 +3,7 @@
     class="mb-4 flex items-center rounded-lg border border-gray-400 py-2 px-2"
   >
     <BaseIconSvg
-      icon="/icon/default/info-icon.svg"
+      icon="/icon/default/Info-icon.svg"
       class="!h-[20px] !w-[20px]"
       fill-color="#1E88E5"
     />
