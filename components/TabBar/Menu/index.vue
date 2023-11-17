8361a79a2963703c3f6cf23c8029963983b64e2d
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-11-17 09:33:34 +0700

    feat : add component tab bar menu

---
+++ components/TabBar/Menu/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <BaseTabList
+    class="h-12 bg-white"
+    :list-tab="listTab"
+    @selected="selectedTabHandle"
+  >
+    <template #default="{ dataTab, indexTab }">
+      <button
+        :class="{
+          'border-b border-b-[3px] border-green-700':
+            indexTab === selectedTabIndex,
+        }"
+        class="!ml-4 !mr-2 !w-fit"
+      >
+        <BaseTab
+          class="!h-[45px] !min-w-[102px] rounded-none !bg-white !py-[10px] !px-2"
+          :selected="indexTab === selectedTabIndex"
+          :title="dataTab.name"
+        >
+          <div
+            class="text-sm font-medium text-gray-700"
+            :class="{
+              '!font-bold !text-green-700': indexTab === selectedTabIndex,
+            }"
+          >
+            {{ dataTab.name }}
+          </div>
+        </BaseTab>
+      </button>
+    </template>
+  </BaseTabList>
+</template>
+
+<script>
+export default {
+  name: 'TabBarMenu',
+  props: {
+    listTab: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      selectedTabIndex: 0
+    }
+  },
+  methods: {
+    selectedTabHandle (index) {
+      this.selectedTabIndex = index
+    }
+  }
+}
+</script>
