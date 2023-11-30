3e2a0d5041e97d3c2f90b2236f29dfa48b3342d9
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-11-30 16:03:49 +0700

    feat : add dialog view document

---
+++ components/Aduan/Dialog/ViewDocument/ListDocument/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="flex items-center justify-between">
+    <div class="flex">
+      <img
+        :src="require(`~/assets/images/${getImageFileType()}`)"
+        :alt="`file ${fileDocument.type}`"
+        height="32"
+        width="25"
+        class="flex-shrink-0"
+      >
+      <span class="font-sans-2 ml-3">{{ fileDocument.name }}</span>
+    </div>
+
+    <jds-button
+      variant="secondary"
+      class="!w-[106px] !rounded-full !text-[14px] !font-bold"
+    >
+      <div class="flex items-center">
+        <BaseIconSvg
+          :icon="`/icon/default/download.svg`"
+          :size="20"
+          fill-color="#069550"
+          class="mr-[10px] flex-shrink-0"
+        />
+        Unduh
+      </div>
+    </jds-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListImage',
+  props: {
+    fileDocument: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  methods: {
+    getImageFileType () {
+      switch (this.fileDocument.type) {
+        case 'pdf':
+          return 'file-pdf.svg'
+        case 'doc':
+          return 'file-doc.svg'
+        case 'excel':
+          return 'file-excel.svg'
+        default:
+          return '-'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.list-document .jds-badge__indicator {
+  @apply !h-[18px] !w-fit !bg-gray-100 !font-roboto !text-xs !font-semibold !text-gray-600;
+}
+</style>
---
+++ components/Aduan/Dialog/ViewDocument/ListImage/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="flex items-center justify-between">
+    <jds-badge
+      :value="`${listImage.length} Terlampir`"
+      position="right"
+      align="top"
+      inline
+      :dot="false"
+      size="md"
+      class="list-document"
+    >
+      <img
+        src="~/assets/images/file-image.svg"
+        alt="file image"
+        height="32"
+        width="25"
+      >
+      <span class="font-sans-2 ml-3">Gambar</span>
+    </jds-badge>
+    <div class="flex items-center">
+      <div class="mr-3">
+        <jds-button
+          label="Lihat"
+          variant="secondary"
+          class="!w-[68px] !rounded-full !text-[14px] !font-bold"
+          @click="isShowPopupViewImage = true"
+        />
+      </div>
+
+      <jds-button
+        variant="secondary"
+        class="!w-[106px] !rounded-full !text-[14px] !font-bold"
+        @click="goToBackHandle()"
+      >
+        <div class="flex items-center">
+          <BaseIconSvg
+            :icon="`/icon/default/download.svg`"
+            :size="20"
+            fill-color="#069550"
+            class="mr-[10px] flex-shrink-0"
+          />
+          Unduh
+        </div>
+      </jds-button>
+    </div>
+    <DialogViewImage
+      :list-photo="listImage"
+      :show-popup="isShowPopupViewImage"
+      @close="isShowPopupViewImage = false"
+    />
+  </div>
+</template>
+
+<script>
+import DialogViewImage from '~/components/Aduan/DialogViewImage'
+export default {
+  name: 'ListImage',
+  components: { DialogViewImage },
+  props: {
+    listImage: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      isShowPopupViewImage: false
+    }
+  }
+}
+</script>
+
+<style>
+.list-document .jds-badge__indicator {
+  @apply !w-fit !bg-gray-100 px-[5px] !text-gray-600;
+}
+
+.list-document .jds-badge__indicator span {
+  @apply !font-roboto !text-xs !font-medium !leading-[18px];
+}
+</style>
