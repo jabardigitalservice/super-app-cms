989d5a1a91daaf2345ec0014ee22a4f548dde929
marsellavaleria19 <marsellavaleria19@gmail.com>
2023-11-30 15:01:50 +0700

    refactor : add slot on component base dialog header

--- components/Base/Dialog/Header/index.vue
+++ components/Base/Dialog/Header/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="px-6 py-2">
-    <h1 class="text-[21px] text-green-800 font-bold font-roboto">
-      {{ title }}
-    </h1>
+    <slot>
+      <h1 class="text-[21px] text-green-800 font-bold font-roboto">
+        {{ title }}
+      </h1>
+    </slot>
   </div>
 </template>
 
