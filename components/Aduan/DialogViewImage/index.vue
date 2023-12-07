65d807dd4c0659f3c86e66f856716f79d99c5c85
Ray Fajar Salinggih <58242304+rayfajars@users.noreply.github.com>
2023-12-07 15:07:06 +0700

    feat: add download image one on one

--- components/Aduan/DialogViewImage/index.vue
+++ components/Aduan/DialogViewImage/index.vue
@@ -1,39 +1,71 @@
 <template>
   <BaseDialog :show-popup="showPopup">
-    <BaseDialogPanel class="w-[1240px] h-[720px] px-10">
-      <BaseDialogHeader title="Bukti Foto" />
+    <BaseDialogPanel class="h-[520px] w-[1240px]">
+      <BaseDialogHeader title="Bukti Foto" class="mb-4 flex justify-between">
+        <h1 class="font-roboto text-[21px] font-bold text-green-800">
+          Bukti Foto
+        </h1>
+        <button @click="$emit('close')">
+          <BaseIconSvg
+            icon="/icon/default/cross.svg"
+            class="!h-6 !w-6"
+            fill-color="#212121"
+          />
+        </button>
+      </BaseDialogHeader>
       <swiper
         :slides-per-view="3"
-        :space-between="16"
+        :space-between="30"
         :centered-slides="true"
         observer
         observe-parents
         class="!pt-[50px]"
         @swiper="getSwipperRefHandle"
       >
-        <swiper-slide v-for="(item,index) in listPhoto" :key="index" class="!h-[520px] w-[390px]">
+        <swiper-slide
+          v-for="(item, index) in listPhoto"
+          :key="index"
+          class="!h-[320px]"
+        >
           <img
             :src="item.url"
             alt="photo"
-            class="w-full h-full rounded-[4px]"
-            :class="{'opacity-20':index!==swiperRef?.activeIndex}"
-            @click="downloadImageHandle(item.url)"
+            class="h-full w-full rounded-[4px]"
+            :class="{ 'opacity-20': index !== swiperRef?.activeIndex }"
           >
         </swiper-slide>
       </swiper>
-      <footer class="flex justify-center items-center pt-10 pb-5">
+      <footer class="flex items-center justify-center pt-7 pb-5">
         <div class="mr-3">
-          <jds-button class="!bg-green-600 !rounded-full" @click="swipperImageHandle('prev')">
+          <jds-button
+            class="!rounded-full !bg-green-600"
+            @click="swipperImageHandle('prev')"
+          >
             <jds-icon name="chevron-left" size="xs" fill="#FFFFFF" />
           </jds-button>
         </div>
         <div class="mr-3">
-          <jds-button variant="secondary" class="!bg-white !border-2 !border-gray-200 !rounded-full" @click="$emit('close')">
-            <BaseIconSvg icon="/icon/default/cross.svg" :size="20" fill-color="#757575" class="mt-[2px]" />
+          <jds-button
+            variant="secondary"
+            class="!rounded-full !border-2 !border-gray-200 !bg-white"
+            @click="downloadImageHandle()"
+          >
+            <div class="flex items-center">
+              <BaseIconSvg
+                :icon="`/icon/default/download.svg`"
+                :size="20"
+                fill-color="#069550"
+                class="mr-[10px] flex-shrink-0"
+              />
+              Unduh
+            </div>
           </jds-button>
         </div>
         <div>
-          <jds-button class="!bg-green-600 !rounded-full" @click="swipperImageHandle('next')">
+          <jds-button
+            class="!rounded-full !bg-green-600"
+            @click="swipperImageHandle('next')"
+          >
             <jds-icon name="chevron-right" size="xs" fill="#FFFFFF" />
           </jds-button>
         </div>
@@ -43,6 +75,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import { Navigation, Pagination, Autoplay } from 'swiper'
 import { SwiperCore, Swiper, SwiperSlide } from 'swiper-vue2'
 import 'swiper/swiper-bundle.css'
@@ -82,35 +115,28 @@ export default {
           this.swiperRef.slideTo(this.swiperRef?.activeIndex - 1)
         }
       }
+    },
+    async downloadImageHandle () {
+      if (this.swiperRef) {
+        const activeIndex = this.swiperRef.activeIndex
+        const activeItem = this.listPhoto[activeIndex]
+
+        try {
+          const imageUrl = activeItem.url
+          const splitUrl = imageUrl.split('/')
+          const imageName = splitUrl[splitUrl.length - 1]
+          const response = await axios.get(imageUrl, { responseType: 'blob' })
+          const link = document.createElement('a')
+          link.href = window.URL.createObjectURL(new Blob([response.data]))
+          link.download = imageName
+          document.body.appendChild(link)
+          link.click()
+          document.body.removeChild(link)
+        } catch (error) {
+          console.error('Gagal mendownload gambar', error)
+        }
+      }
     }
-    /* To download images, but postpone it first and will use it if the image URL from the API is no longer subject to CORS
-    downloadImageHandle (urlImage) {
-      // this.dataDialog.title = 'Download Bukti Foto'
-      // fetch('https://dvgddkosknh6r.cloudfront.net/live/media/img/1687855390-banner-mpp.jpg').then(response => console.log(response)).catch(error => console.log(error))
-      // try {
-      //   const response = await fetch(urlImage, {
-      //     mode: 'no-cors'
-      //   })
-      //   console.log(response)
-      //   const blobImage = await response.blob()
-      //   console.log(blobImage)
-      //   const href = URL.createObjectURL(blobImage)
-      //   const anchorElement = document.createElement('a')
-      //   anchorElement.href = href
-      //   anchorElement.download = 'myimage.png'
-      //   anchorElement.click()
-      // } catch (error) {
-      //   console.log(error)
-      // }
-    } */
   }
 }
 </script>
-
-<style scoped>
-/* .swiper-container{
-  width: 100%;
-  height: 100%;
-  padding-top: 26px;
-} */
-</style>
