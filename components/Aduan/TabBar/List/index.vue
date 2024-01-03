527d6b37a95e4a3a7479ac10574c77c0209e637b
Ray Fajar Salinggih <58242304+rayfajars@users.noreply.github.com>
2024-01-03 16:23:57 +0700

    feat: add history tab

--- components/Aduan/TabBar/List/index.vue
+++ components/Aduan/TabBar/List/index.vue
@@ -11,43 +11,43 @@
       >
         <BaseTab
           class="flex items-start text-sm text-green-100"
-          :selected="indexTab === selectedTabIndex"
+          :selected="indexTab === tabIndex"
           :title="dataTab.name"
         >
           <div
             class="h-[28px] w-[28px] rounded-full p-1"
             :class="
-              indexTab === selectedTabIndex ? 'bg-gray-100' : 'bg-green-800'
+              indexTab === tabIndex ? 'bg-gray-100' : 'bg-green-800'
             "
           >
             <BaseIconSvg
               :icon="dataTab.icon"
               class="icon-tab-content !h-[14px] !w-[14px] !shadow-lg"
               :fill-color="
-                indexTab === selectedTabIndex ? '#16A75C' : '#FFFFFF'
+                indexTab === tabIndex ? '#16A75C' : '#FFFFFF'
               "
               :class="{
-                'icon-tab-selected': indexTab === selectedTabIndex,
+                'icon-tab-selected': indexTab === tabIndex,
               }"
             />
           </div>
 
           <div
             class="ml-2 !font-roboto text-green-100"
-            :class="{ '!text-gray-700': indexTab === selectedTabIndex }"
+            :class="{ '!text-gray-700': indexTab === tabIndex }"
           >
             {{ dataTab.name }}
             <p
               class="text-left !font-roboto text-xl font-medium text-white"
               :class="{
-                '!text-blue-gray-800': indexTab === selectedTabIndex,
+                '!text-blue-gray-800': indexTab === tabIndex,
               }"
             >
               {{ dataTab.value }}
               <span
                 class="!font-roboto text-sm font-medium capitalize text-white"
                 :class="{
-                  '!text-blue-gray-800': indexTab === selectedTabIndex,
+                  '!text-blue-gray-800': indexTab === tabIndex,
                 }"
               >{{ dataTab.unit }}</span>
             </p>
@@ -69,6 +69,10 @@ export default {
     typeAduan: {
       type: String,
       default: ''
+    },
+    tabIndex: {
+      type: Number,
+      default: 0
     }
   },
   data () {
@@ -79,6 +83,7 @@ export default {
   methods: {
     selectedTabHandle (index) {
       this.selectedTabIndex = index
+      this.$emit('selected', index)
     }
   }
 }
