154f2e6b9aadbad785a204e430bdcfa1fc0240c4
Ray Fajar Salinggih <rayfajars@gmail.com>
2024-01-15 15:25:03 +0700

    feat: detail message notif fetch

--- pages/message-notif/detail/_id/index.vue
+++ pages/message-notif/detail/_id/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <MessageNotifDetail :detail-message-notif="detailMessageNotif" @fetchData=" $fetch()" />
+  <MessageNotifDetail
+    :detail-message-notif="detailMessageNotif"
+    @fetchData="$fetch()"
+  />
 </template>
 
 <script>
@@ -25,10 +28,20 @@ export default {
   },
   async fetch () {
     try {
-      const response = await this.$axios.get(`/messages/${this.$route.params.id}`)
+      const response = await this.$axios.get(
+        `/messages/${this.$route.params.id}`
+      )
       this.detailMessageNotif = response.data.data
-      this.detailMessageNotif.createdAt = formatDate(this.detailMessageNotif.createdAt || '', 'dd/MM/yyyy HH:mm') || '-'
-      this.detailMessageNotif.publishedAt = formatDate(this.detailMessageNotif.publishedAt || '', 'dd/MM/yyyy HH:mm') || '-'
+      this.detailMessageNotif.createdAt =
+        formatDate(
+          this.detailMessageNotif.createdAt || '',
+          'dd/MM/yyyy HH:mm'
+        ) || '-'
+      this.detailMessageNotif.publishedAt =
+        formatDate(
+          this.detailMessageNotif.publishedAt || '',
+          'dd/MM/yyyy HH:mm'
+        ) || '-'
       this.descriptionPage = `Detail dari pesan “${this.detailMessageNotif.title}”`
     } catch (error) {
       this.detailMessageNotif = {}
