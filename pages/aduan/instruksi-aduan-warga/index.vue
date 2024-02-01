4678a377e95b574a684c5d9fb0910a89cef4c395
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-02-01 15:47:09 +0700

    feat : add page list instruction of citizen complaint

---
+++ pages/aduan/instruksi-aduan-warga/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <DaftarIkp :ikp-type-page="ikpType.instruksiAduanWarga.props" />
+</template>
+
+<script>
+import DaftarIkp from '~/components/Aduan/DaftarIKP'
+import { ikpType } from '~/constant/daftar-ikp'
+export default {
+  name: 'PageInstruksiAduan',
+  components: { DaftarIkp },
+  layout: 'Dashboard',
+  data () {
+    return {
+      ikpType,
+      navigations: [
+        {
+          label: ikpType.instruksiAduanWarga.label,
+          link: ikpType.instruksiAduanWarga.link
+        }
+      ],
+      descriptionPage: 'Berisi semua daftar instruksi aduan warga dari masyarakat Jabar yang perlu ditindaklanjuti.'
+    }
+  },
+  mounted () {
+    this.$store.commit('setActivePage', 'Daftar Instruksi Aduan')
+    this.$store.commit('setHeader', {
+      navigations: this.navigations,
+      descriptionPage: this.descriptionPage
+    })
+  }
+}
+</script>
