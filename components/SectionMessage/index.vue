ce968fd9b10ca04f117d7242b416b8ebae39255f
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-04-23 14:48:16 +0700

    feat: add component section message

---
+++ components/SectionMessage/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="h-[78px] rounded-lg border border-blue-800 bg-blue-50 p-4">
+    <div class="flex items-start">
+      <BaseIconSvg
+        :icon="icon.path"
+        size="20"
+        class="flex-shrink-0"
+        :fill-color="icon.fill"
+      />
+      <p class="ml-3 font-lato text-sm">
+        {{ message }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SectionMessage',
+  props: {
+    message: {
+      type: String,
+      default: '',
+    },
+    icon: {
+      type: Object,
+      default: () => ({
+        path: '/icon/default/Info-icon.svg',
+        fill: '#1E88E5',
+      }),
+    },
+  },
+}
+</script>
