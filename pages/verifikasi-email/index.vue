e661c6bd7328137b4afca16ba0d24e5d3ee8a224
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-05-06 17:03:47 +0700

    refactor: change path import component email verification

--- pages/verifikasi-email/index.vue
+++ pages/verifikasi-email/index.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import EmailVerification from '~/components/aduan/EmailVerification'
+import EmailVerification from '~/components/Aduan/EmailVerification'
 
 export default {
   name: 'PageEmailVerification',
