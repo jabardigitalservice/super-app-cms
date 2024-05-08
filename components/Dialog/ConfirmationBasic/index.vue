96eb7fc3b933232d8fb517337e03eaea61d0f258
Rizki Firmansyah <rizfirman@gmail.com>
2024-05-08 14:45:00 +0700

    create baseConfirmationDialogBasic and improve dialogBatalkanReservasi

---
+++ components/Dialog/ConfirmationBasic/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <BaseDialogFrame :name="dialogModal?.nameModal">
+    <BaseDialogPanel class="w-[510px]">
+      <BaseDialogHeader :title="dialogModal?.title" />
+      <BaseDialogDescription
+        :description="dialogModal?.descriptionText"
+        :sub-description="detailItemModal.title"
+        class="p-6"
+      />
+      <BaseDialogFooterNew :name="`${dialogModal?.nameModal}`">
+        <template #button-right>
+          <jds-button
+            :label="dialogModal?.button.label"
+            type="button"
+            :variant="dialogModal?.button.variant"
+            @click.prevent="$emit('confirmation-button')"
+          />
+        </template>
+
+        <slot name="footer-custom" />
+      </BaseDialogFooterNew>
+    </BaseDialogPanel>
+  </BaseDialogFrame>
+</template>
+
+<script>
+export default {
+  name: 'DialogConfirmationBasic',
+  props: {
+    dialogModal: { type: Object, default: () => ({}) },
+    detailItemModal: { type: Object, default: () => ({}) },
+  },
+}
+</script>
