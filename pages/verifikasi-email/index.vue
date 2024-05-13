062564fec974658c7a0b8171530d8c4e0253e71c
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-05-13 07:32:02 +0700

    refactor:integration email verification

--- pages/verifikasi-email/index.vue
+++ pages/verifikasi-email/index.vue
@@ -1,10 +1,30 @@
 <template>
-  <EmailVerification verification-type="failed" email="test@mail.com" />
+  <EmailVerification
+    :verification-type="isAuthed ? 'success' : 'failed'"
+    :email="email"
+    :link-page="link"
+  />
 </template>
 
 <script>
 export default {
   name: 'PageEmailVerification',
   layout: 'Verification',
+  async asyncData({ $axios, query }) {
+    const queryToken = query.token
+    const dataDecode = Buffer.from(queryToken, 'base64').toString('utf-8')
+    const token = dataDecode.split(':')[0]
+    const userId = dataDecode.split(':')[1]
+    const isAuthed = dataDecode.split(':')[2] === 'true'
+    const email = dataDecode.split(':')[3]
+    const link = isAuthed
+      ? '/login'
+      : Buffer.from(dataDecode.split(':')[4], 'base64').toString('utf-8')
+    await $axios.post('/users/admin/complaint/verify-invitation', {
+      token,
+      user_id: userId,
+    })
+    return { isAuthed, email, link }
+  },
 }
 </script>
