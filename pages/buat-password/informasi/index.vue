9c63dfdd9bedc8b9b15553c44bdfb331fec0c90b
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-05-21 15:29:24 +0700

    feat : slicing layout page create password-information

---
+++ pages/buat-password/informasi/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <CreatePasswordInformation
+      :information-type="isSuccess ? 'success' : 'failed'"
+      :email="email"
+      :link="link"
+    />
+  </div>
+</template>
+
+<script>
+import CreatePasswordInformation from '~/components/CreatePassword/Information'
+export default {
+  name: 'PageCreatePasswordInformation',
+  auth: false,
+  components: { CreatePasswordInformation },
+  layout: 'Verification',
+  data() {
+    return {
+      email: '',
+      link: '#',
+      isSuccess: false,
+    }
+  },
+  mounted() {
+    const queryToken = this.$route.query?.token
+    const dataDecode = Buffer.from(queryToken, 'base64').toString('utf-8')
+    this.isSuccess = dataDecode.split(':')[0] === 'true'
+    let token = ''
+    if (this.isSuccess) {
+      this.email = dataDecode.split(':')[1] || ''
+    } else {
+      token = dataDecode.split(':')[1] || ''
+    }
+    this.link = this.isSuccess ? '/login' : `/buat-password?token=${token}`
+  },
+}
+</script>
