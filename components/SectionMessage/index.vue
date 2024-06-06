ddce86ced2a745149eeac5fb19a7b5ad22c5b83d
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-06-06 13:54:45 +0700

    refactor: add button close on component section message

--- components/SectionMessage/index.vue
+++ components/SectionMessage/index.vue
@@ -2,19 +2,29 @@
   <div
     class="section-message h-fit rounded-lg border border-blue-800 bg-blue-50 p-4"
   >
-    <div class="flex items-start">
-      <BaseIconSvg
-        :icon="icon.path"
-        :size="icon.size"
-        class="flex-shrink-0"
-        :fill-color="icon.fill"
-      />
-      <div class="description ml-3">
-        <p class="font-lato text-sm">
-          {{ message }}
-        </p>
-        <strong class="text-sm">{{ detailItem }}</strong>
+    <div class="flex items-start justify-between">
+      <div class="flex">
+        <BaseIconSvg
+          :icon="icon.path"
+          :size="icon.size"
+          class="flex-shrink-0"
+          :fill-color="icon.fill"
+        />
+        <div class="description ml-3">
+          <p class="font-lato text-sm">
+            {{ message }}
+          </p>
+          <strong v-if="detailItem" class="text-sm">{{ detailItem }}</strong>
+        </div>
       </div>
+      <button v-if="isShowButtonClose" @click="$emit('close')">
+        <BaseIconSvg
+          :icon="buttonClose.path"
+          :size="buttonClose.size"
+          class="flex-shrink-0"
+          :fill-color="buttonClose.fill"
+        />
+      </button>
     </div>
   </div>
 </template>
@@ -39,6 +49,19 @@ export default {
         fill: '#1E88E5',
       }),
     },
+    isShowButtonClose: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      buttonClose: {
+        path: '/icon/default/cross.svg',
+        size: this.icon.size,
+        fill: this.icon.fill,
+      },
+    }
   },
 }
 </script>
