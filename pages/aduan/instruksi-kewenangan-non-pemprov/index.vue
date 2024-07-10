b9353a1c7fd34c1ecd3912f5890f5c63177659b0
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-07-10 07:27:55 +0700

    feat : add page home feature non government authority instruction

---
+++ pages/aduan/instruksi-kewenangan-non-pemprov/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Aduan
+      :type-aduan-page="typeAduan.instruksiKewenanganNonPemprov.props"
+      link-page-detail="/aduan/instruksi-kewenangan-anon-pemprov/detail"
+    />
+  </div>
+</template>
+
+<script>
+import { typeAduan } from '~/constant/aduan-masuk.js'
+
+export default {
+  name: 'PageInstruksiKewenanganPemprov',
+  layout: 'Dashboard',
+  data() {
+    return {
+      navigations: [
+        {
+          label: typeAduan.instruksiKewenanganNonPemprov.label,
+          link: typeAduan.instruksiKewenanganNonPemprov.link,
+        },
+      ],
+      descriptionPage:
+        'Berisi semua daftar aduan dari masyarakat Jabar yang perlu untuk dikoordinasikan.',
+      typeAduan,
+    }
+  },
+  mounted() {
+    this.$store.commit('setActivePage', 'Instruksi Kewenangan Non-Pemprov')
+    this.$store.commit('setHeader', {
+      navigations: this.navigations,
+      descriptionPage: this.descriptionPage,
+    })
+  },
+}
+</script>
