deae839d76f263edb276c2f80c6bff47f71acf63
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-07-12 10:32:59 +0700

    feat : add page detail complaint non government authority instruction

---
+++ pages/aduan/instruksi-kewenangan-non-pemprov/detail/_id/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <AduanDetail
+    :type-aduan-page="typeAduan.instruksiKewenanganNonPemprov.props"
+  />
+</template>
+
+<script>
+import { typeAduan } from '~/constant/aduan-masuk.js'
+export default {
+  name: 'PageDetailInstruksiKewenanganNonPemprov',
+  layout: 'Dashboard',
+  data() {
+    return {
+      typeAduan,
+      navigations: [
+        {
+          label: this.$route.query.fromInstructionPage
+            ? '...'
+            : typeAduan.instruksiKewenanganNonPemprov.label,
+          link: typeAduan.instruksiKewenanganNonPemprov.link,
+          disabled: true,
+        },
+        {
+          label: 'Detail Instruksi',
+          link: `/aduan/instruksi-kewenangan-non-pemprov/detail-ikp/${this.$route.query?.ikpCode}`,
+          disabled: true,
+        },
+        {
+          label: 'Detail Aduan',
+          link: `/aduan/instruksi-kewenangan-non-pemprov/detail/${this.$route.params.id}`,
+        },
+      ],
+      descriptionPage:
+        'Berisi detail aduan dari masyarakat Jabar yang perlu untuk dikoordinasikan.',
+    }
+  },
+  mounted() {
+    const dataHeader = {
+      label: typeAduan.instruksiKewenanganNonPemprov.label,
+      navigations: this.navigations,
+      descriptionPage: this.descriptionPage,
+      fromInstructionPage: this.$route.query?.fromInstructionPage,
+    }
+    this.$store.dispatch('updateNavInstruction', dataHeader)
+  },
+}
+</script>
