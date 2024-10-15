c35fd30fa33b680e36b6cc3b71b5d3edf3f660bc
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-10-15 17:17:10 +0700

    fix : fixing popover content on base component button dropdown

--- components/Base/ButtonDropdown/index.vue
+++ components/Base/ButtonDropdown/index.vue
@@ -6,37 +6,29 @@
         class="h-fit w-full border border-green-600 bg-green-700 py-[8px] px-[16px] text-[14px] font-medium text-white"
         @click="isShowPopOver = !isShowPopOver"
       >
-        <div class="flex items-center justify-between">
-          <slot />
-          <jds-icon
-            name="chevron-down"
-            fill="#FFFFFF"
-            size="14px"
-            class="ml-[10px]"
-            :class="{ 'rotate-180': isShowPopOver }"
-          />
-        </div>
+        <slot>
+          <div class="flex items-center justify-between">
+            {{ label }}
+            <jds-icon
+              name="chevron-down"
+              fill="#FFFFFF"
+              size="14px"
+              class="ml-[10px]"
+              :class="{ 'rotate-180': isShowPopOver }"
+            />
+          </div>
+        </slot>
       </BaseButton>
     </template>
     <div
-      class="mt-1 w-fit rounded-lg border border-gray-400 bg-white p-[16px] shadow-xl"
+      class="mt-1 mr-6 w-fit rounded-lg border border-gray-400 bg-white p-[16px] shadow-xl"
     >
-      <div class="mb-2 border-b">
-        <h5 class="text-center font-lato text-[16px] font-bold">
-          Pilih Tipe File
-        </h5>
-      </div>
-
       <div
         v-for="(menuPopOver, index) in listMenuPopOver"
         :key="index"
         :class="{ 'mb-4': index !== listMenuPopOver.length - 1 }"
       >
-        <button
-          :key="index"
-          class="font-lato text-[14px] font-normal"
-          @click="$emit(`${menuPopOver.value}`)"
-        >
+        <button :key="index" @click="$emit(`${menuPopOver.value}`)">
           {{ menuPopOver.menu }}
         </button>
       </div>
@@ -54,6 +46,10 @@ export default {
     listMenuPopOver: {
       type: Array,
       default: () => [],
+    },
+    label: {
+      type: String,
+      default: '',
     },
   },
   data() {
