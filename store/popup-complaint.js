fcc625cdd1ea57438d56b38f5ee0fe43fd77871e
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-10-20 22:17:49 +0700

    feat : add state management popup complaint

---
+++ store/popup-complaint.js
@@ -0,0 +1,66 @@
+export const state = () => ({
+  detailItem: {},
+  dataDialog: {
+    title: '',
+    nameModal: '',
+    descriptionText: '',
+    button: {
+      label: '',
+      variant: '',
+    },
+  },
+  isMockApi: false,
+  isSuccess: false,
+})
+
+export const actions = {
+  showPopupFollowupHotlineJabar({ commit }, { dataComplaint, dialogName }) {
+    commit('setDataComplaint', dataComplaint)
+    commit('modals/OPEN', dialogName, { root: true })
+  },
+  async integrationApi(
+    { state, commit },
+    { dataApi, payload, dataDialogSuccess, dataDialogFailed }
+  ) {
+    commit('modals/CLOSEALL', null, { root: true })
+    try {
+      // using mock api
+      if (state.isMockApi) {
+        await this.$mockApi[dataApi.method](dataApi.url, { ...payload })
+      } else {
+        await this.$axios[dataApi.method](dataApi.url, { ...payload })
+      }
+      commit('setDataDialog', {
+        nameModal: dataDialogSuccess.nameModal,
+        dialogModal: {
+          ...dataDialogSuccess.dialogModal,
+          icon: { name: 'check-mark-circle', fill: '#069550' },
+        },
+      })
+      commit('setIsSuccess', true)
+      commit('modals/OPEN', state.dataDialog.nameModal, { root: true })
+    } catch {
+      commit('setDataDialog', {
+        nameModal: dataDialogFailed.nameModal,
+        dialogModal: {
+          ...dataDialogFailed.dialogModal,
+          icon: { name: 'times-circle', fill: '#EF5350' },
+        },
+      })
+      commit('setIsSuccess', false)
+      commit('modals/OPEN', dataDialogFailed.nameModal, { root: true })
+    }
+  },
+}
+
+export const mutations = {
+  setDataDialog(state, dataDialog) {
+    state.dataDialog = dataDialog
+  },
+  setIsMockApi(state, isMockApi) {
+    state.isMockApi = isMockApi
+  },
+  setIsSuccess(state, isSuccess) {
+    state.isSuccess = isSuccess
+  },
+}
