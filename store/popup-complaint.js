1b3db0d1ed301b52696aad583106135afadef73f
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-10-23 07:08:01 +0700

    refactor : remove some state & mutation on state management popup complaint

--- store/popup-complaint.js
+++ store/popup-complaint.js
@@ -1,11 +1,6 @@
 export const state = () => ({
-  detailItem: {},
-  dataDialogConfirmation: {},
-  dataDialogInformation: {},
   dataComplaint: {},
   isMockApi: false,
-  isSuccess: false,
-  isLoading: false,
 })
 
 export const actions = {
@@ -13,43 +8,22 @@ export const actions = {
     commit('setDataComplaint', dataComplaint)
     commit('modals/OPEN', dialogName, { root: true })
   },
-  backToForm({ commit }, dialogName) {
-    commit('modals/CLOSEALL', { root: true })
-    commit('modals/OPEN', dialogName, { root: true })
-  },
-  async integrationApi(
-    { state, commit },
-    { dataApi, payload, dataDialogSuccess, dataDialogFailed }
-  ) {
-    commit('modals/CLOSEALL', null, { root: true })
+  async integrationApi({ state }, { dataApi, payload }) {
+    // eslint-disable-next-line no-useless-catch
     try {
-      // using mock api
+      let response = ''
       if (state.isMockApi) {
-        await this.$mockApi[dataApi.method](dataApi.url, { ...payload })
+        response = await this.$mockApi[dataApi.method](dataApi.url, {
+          ...payload,
+        })
       } else {
-        await this.$axios[dataApi.method](dataApi.url, { ...payload })
+        response = await this.$axios[dataApi.method](dataApi.url, {
+          ...payload,
+        })
       }
-      commit('setDataDialogInformation', {
-        nameModal: dataDialogSuccess.nameModal,
-        dialogModal: {
-          ...dataDialogSuccess.dialogModal,
-          icon: { name: 'check-mark-circle', fill: '#069550' },
-        },
-      })
-      commit('setIsSuccess', true)
-      commit('modals/OPEN', dataDialogSuccess.nameModal, { root: true })
-    } catch {
-      commit('setDataDialogInformation', {
-        nameModal: dataDialogFailed.nameModal,
-        dialogModal: {
-          ...dataDialogFailed.dialogModal,
-          icon: { name: 'times-circle', fill: '#EF5350' },
-        },
-      })
-      commit('setIsSuccess', false)
-      commit('modals/OPEN', dataDialogFailed.nameModal, { root: true })
-    } finally {
-      commit('setIsLoading', false)
+      return response
+    } catch (error) {
+      throw error
     }
   },
 }
@@ -58,19 +32,7 @@ export const mutations = {
   setDataComplaint(state, dataComplaint) {
     state.dataComplaint = dataComplaint
   },
-  setDataDialogConfirmation(state, dataDialogConfirmation) {
-    state.dataDialogConfirmation = dataDialogConfirmation
-  },
-  setDataDialogInformation(state, dataDialogInformation) {
-    state.dataDialogInformation = dataDialogInformation
-  },
   setIsMockApi(state, isMockApi) {
     state.isMockApi = isMockApi
-  },
-  setIsSuccess(state, isSuccess) {
-    state.isSuccess = isSuccess
-  },
-  setIsLoading(state, isLoading) {
-    state.isLoading = isLoading
   },
 }
