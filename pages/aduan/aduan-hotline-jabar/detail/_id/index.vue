b2b3d2d711ee82ea622f33dc92573a2d1b0ac4c9
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-10-23 14:28:31 +0700

    feat : add page detial hotline jabar

---
+++ pages/aduan/aduan-hotline-jabar/detail/_id/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <AduanDetail
+    :type-aduan-page="typeAduan.aduanDialihkanHotlineJabar.props"
+    :list-button="listButtonDetail"
+  />
+</template>
+
+<script>
+import { typeAduan, complaintButtonDetail } from '~/constant/aduan-masuk'
+import popupAduanMasuk from '~/mixins/popup-aduan-masuk'
+
+export default {
+  name: 'PageDetailAduanHotlineJabar',
+  mixins: [popupAduanMasuk],
+  layout: 'Dashboard',
+  data() {
+    return {
+      navigations: [
+        {
+          label: typeAduan.aduanDialihkanHotlineJabar.label,
+          link: typeAduan.aduanDialihkanHotlineJabar.link,
+        },
+        {
+          label: 'Detail Aduan',
+          link: `/aduan/aduan-hotline-jabar/detail/${this.$route.params.id}`,
+        },
+      ],
+      descriptionPage: 'Berisi detail aduan dari masyarakat Jabar',
+      typeAduan,
+      listButtonDetail: [
+        complaintButtonDetail.followupHotlineJabar,
+        complaintButtonDetail.evidenceFollowupHotlineJabar,
+      ],
+    }
+  },
+  mounted() {
+    this.$store.commit('setActivePage', typeAduan.aduanMasuk.label)
+    this.$store.commit('setHeader', {
+      navigations: this.navigations,
+      descriptionPage: this.descriptionPage,
+    })
+  },
+}
+</script>
