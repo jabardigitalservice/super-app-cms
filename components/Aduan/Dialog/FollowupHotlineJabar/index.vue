bbed85995869c59a9aae5dcdf472ef947741fc12
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-10-29 22:56:26 +0700

    fix : change url logo complaint source using api on dialog form followup hotline

--- components/Aduan/Dialog/FollowupHotlineJabar/index.vue
+++ components/Aduan/Dialog/FollowupHotlineJabar/index.vue
@@ -24,9 +24,7 @@
                       }"
                     >
                       <img
-                        :src="
-                          require(`~/assets/logo/${dataComplaint?.complaint_source?.logo}`)
-                        "
+                        :src="dataComplaint?.complaint_source?.logo"
                         alt="logo"
                         width="11"
                         height="15"
