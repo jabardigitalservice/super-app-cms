d6f21a99cc0e20ad9ce8a0038679d6ad898b0a13
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-11-26 07:35:50 +0700

    fix: layout card dialog-tracking span lapor

--- components/Aduan/Dialog/TrackingSpanLapor/CardSpanLapor/index.vue
+++ components/Aduan/Dialog/TrackingSpanLapor/CardSpanLapor/index.vue
@@ -1,19 +1,21 @@
 <template>
   <div
-    class="rounded-t-lg border border-b-0 border-x-gray-300 last:rounded-b-lg last:!border-b last:border-b-gray-300"
+    class="border border-b-0 border-x-gray-300 last:rounded-b-lg last:!border-b last:border-b-gray-300"
   >
-    <div class="flex h-11 items-center rounded-t-lg bg-[#F4F6F6] p-3">
+    <div class="flex h-11 items-center bg-[#F4F6F6] p-3">
       <jds-icon name="calendar-date-outline" size="sm" fill="#9E9E9E" />
-      <p class="ml-2 font-roboto text-xs text-gray-800">
+      <p class="ml-2 font-roboto text-[12px] leading-5 text-gray-800">
         {{ spanLapor.tracking_at }}
       </p>
     </div>
-    <div class="p-3">
-      <h1 class="text-sm font-medium text-gray-900">
+    <div class="min-h-[128px] p-3">
+      <h1
+        class="text-shadow font-lato text-[14px] font-medium leading-[23px] !text-gray-900"
+      >
         {{ spanLapor.tracking_by }}
       </h1>
-      <h2 class="mt-2 text-sm text-gray-600">Keterangan</h2>
-      <p class="mt-1 text-[14px] leading-[23px] text-gray-800">
+      <h2 class="mt-3 h-[23px] text-sm text-gray-600">Keterangan</h2>
+      <p class="mt-1 text-[14px] font-normal leading-[23px] text-gray-800">
         {{ spanLapor.tracking_note }}
       </p>
     </div>
@@ -43,3 +45,9 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.text-shadow {
+  text-shadow: 0px 1px 1px rgba(0, 0, 0, 0.2);
+}
+</style>
