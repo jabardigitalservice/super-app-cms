fce130ff286fb1ba817fe6e3c6f881f70438d418
marsellavaleria19 <marsellavaleria19@gmail.com>
2024-12-13 09:05:58 +0700

    fix : add form rest when codition valid on form process complaint

--- components/Aduan/Dialog/ProcessComplaint/index.vue
+++ components/Aduan/Dialog/ProcessComplaint/index.vue
@@ -492,8 +492,9 @@ export default {
     },
     async showDialogConfirmation() {
       const isValid = await this.$refs.form.validate()
-      this.$refs.form.reset()
+
       if (isValid) {
+        this.$refs.form.reset()
         this.$store.commit('modals/CLOSEALL')
         this.dialogConfirmation = {
           ...this.dataDialog,
