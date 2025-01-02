0f26362402cccf90e18aa2339e4acc97433b5986
marsellavaleria19 <marsellavaleria19@gmail.com>
2025-01-02 10:30:30 +0700

    feat : add data-cy button direct login

--- pages/login.vue
+++ pages/login.vue
@@ -1,15 +1,27 @@
 <template>
-  <div class="bg-[#E5E5E5] min-h-screen">
-    <div class="flex justify-center items-center h-screen">
-      <div class="h-fit w-[470px] bg-white border border-[#EAEAEF] rounded-lg drop-shadow-md py-[40px] px-[24px]">
+  <div class="min-h-screen bg-[#E5E5E5]">
+    <div class="flex h-screen items-center justify-center">
+      <div
+        class="h-fit w-[470px] rounded-lg border border-[#EAEAEF] bg-white py-[40px] px-[24px] drop-shadow-md"
+      >
         <div class="container mx-auto">
           <HeaderLogin />
           <div class="mt-[44px]">
-            <BaseButton class="bg-green-600 text-white hover:bg-green-700" @click="loginKeyclock">
+            <BaseButton
+              class="bg-green-600 text-white hover:bg-green-700"
+              data-cy="login__button--direct-login-account"
+              @click="loginKeyclock"
+            >
               <template #icon-right>
-                <div class="flex justify-between items-center text-[14px] font-lato font-bold">
+                <div
+                  class="flex items-center justify-between font-lato text-[14px] font-bold"
+                >
                   Login Akun
-                  <jds-icon name="arrow-right" class="w-[15px] h-[12px]" fill="#FFFFFF" />
+                  <jds-icon
+                    name="arrow-right"
+                    class="h-[12px] w-[15px]"
+                    fill="#FFFFFF"
+                  />
                 </div>
               </template>
             </BaseButton>
@@ -25,12 +37,12 @@ import HeaderLogin from '~/components/Login/Header.vue'
 
 export default {
   components: {
-    HeaderLogin
+    HeaderLogin,
   },
   methods: {
-    loginKeyclock () {
+    loginKeyclock() {
       this.$auth.loginWith('keyclock')
-    }
-  }
+    },
+  },
 }
 </script>
