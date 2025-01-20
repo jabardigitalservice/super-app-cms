e54af542127f6c5ad89b256625135a687f4c73ba
Ray Fajar Salinggih <rayfajars@gmail.com>
2025-01-20 08:55:03 +0700

    feat: add loadinfg on list trk and detail aduan

--- components/Aduan/Dialog/FollowupComplaint/index.vue
+++ components/Aduan/Dialog/FollowupComplaint/index.vue
@@ -70,14 +70,8 @@
               @click="showPopupCreateIkp()"
             />
           </div>
-          <div v-if="isLoading">
-            <div class="flex h-[300px] flex-col items-center justify-center">
-              <jds-spinner class="mb-4" size="30" />
-              <p class="font-lato text-base font-bold text-green-700">
-                Loading....
-              </p>
-            </div>
-          </div>
+
+          <BaseLoading v-if="isLoading" />
 
           <template v-else>
             <!-- data IKP -->
