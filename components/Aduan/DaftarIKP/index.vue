a67586253e61f650c1e4f4de88f31d16b1cb3fce
marsellavaleria19 <marsellavaleria19@gmail.com>
2025-01-20 14:51:29 +0700

    fix(instruction-authority-gov) : fix show data instruction by current year on tab ikp

--- components/Aduan/DaftarIKP/index.vue
+++ components/Aduan/DaftarIKP/index.vue
@@ -174,7 +174,9 @@ export default {
       isShowPopupDate: false,
       isShowPopupDateRange: false,
       dateRange: [
-        new Date(new Date().setFullYear(new Date().getFullYear() - 1)),
+        // TODO : it will be used when the data migration process is complete
+        // new Date(new Date().setFullYear(new Date().getFullYear()-1)),
+        new Date(new Date().setDate(1)),
         new Date(),
       ],
       headerDaftarIkp,
@@ -281,6 +283,14 @@ export default {
         this.$fetch()
       }
     }, 500),
+  },
+  created() {
+    this.query = {
+      ...this.query,
+      start_date: formatDate(this.dateRange[0], 'yyyy-MM-dd'),
+      end_date: formatDate(this.dateRange[1], 'yyyy-MM-dd'),
+    }
+    // this.$fetch()
   },
   mounted() {
     this.pagination.itemsPerPageOptions = generateItemsPerPageOptions(
