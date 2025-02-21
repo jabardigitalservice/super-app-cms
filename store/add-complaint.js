438a09fd009b2082e9532c7b507cbd6a03ef7de1
marsellavaleria19 <marsellavaleria19@gmail.com>
2025-02-21 10:59:08 +0700

    refactor(add complaint) : change url api

--- store/add-complaint.js
+++ store/add-complaint.js
@@ -64,7 +64,17 @@ export const actions = {
         ...state.dataOtherComplaint,
         source_id: 'sp4n',
       }
-      await this.$axios.post(ENDPOINT_ADUAN, {
+      // TODO : used when the error information popup is finished testing
+      // await this.$axios.post(ENDPOINT_ADUAN, {
+      //   ...dataAddComplaint,
+      //   user_id: this.$auth?.user?.identifier,
+      //   user_email: this.$auth?.user?.email,
+      //   type: 'private',
+      //   complaint_source: 'sp4n',
+      // })
+
+      // used to test popup error information
+      await this.$axios.post(`${ENDPOINT_ADUAN}/add-complaint`, {
         ...dataAddComplaint,
         user_id: this.$auth?.user?.identifier,
         user_email: this.$auth?.user?.email,
