1f745513f8c271e3fb2693361974be7b1ee0f509
marsellavaleria19 <marsellavaleria19@gmail.com>
2025-10-20 10:40:40 +0700

    feat : add page detail claim kepala desa

---
+++ pages/klaim/kepala-desa/detail/_id/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <KlaimRwDetail
+    :typeClaimPage="typeClaim.klaimKepalaDesa"
+    :list-button="listButtonDetail"
+  />
+</template>
+
+<script>
+import KlaimRwDetail from '~/components/KlaimRW/KlaimRwDetail'
+import { typeClaim } from '~/constant/klaim-rw'
+
+export default {
+  components: { KlaimRwDetail },
+  layout: 'Dashboard',
+  data() {
+    return {
+      navigations: [
+        {
+          label: 'Klaim Akun Kepala Desa',
+          link: '/',
+        },
+        {
+          label: 'Detail Akun Kepala Desa',
+          link: `/detail/${this.$route.params.id}`,
+        },
+      ],
+      typeClaim,
+      listButtonDetail: [
+        {
+          idButton: 'button-claim-verify',
+          label: 'Verifikasi Akun Kepala Desa Ini',
+          variant: 'primary',
+          claimStatus: ['Menunggu Verifikasi', 'Ditolak'],
+        },
+        {
+          idButton: 'button-claim-reject',
+          label: 'Tolak Akun Kepala Desa Ini',
+          variant: 'secondary',
+          classButton: '!border !border-red-400 !text-red-400 hover:!bg-red-50',
+          claimStatus: ['Menunggu Verifikasi'],
+        },
+      ],
+    }
+  },
+  mounted() {
+    this.$store.commit('setActivePage', 'Klaim Akun Kepala Desa')
+    this.$store.commit('setHeader', {
+      navigations: this.navigations,
+      descriptionPage: this.descriptionPage,
+    })
+  },
+}
+</script>
