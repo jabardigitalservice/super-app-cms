14904f011a8f157a7e39a0fbb78533aa02476cd7
marsellavaleria19 <marsellavaleria19@gmail.com>
2025-12-02 09:31:33 +0700

    fix : set active page to "Kegiatan Posyandu"

--- pages/kegiatan/posyandu/index.vue
+++ pages/kegiatan/posyandu/index.vue
@@ -7,7 +7,7 @@ export default {
   name: 'PageActivities',
   layout: 'Dashboard',
   mounted() {
-    this.$store.commit('setActivePage', 'Kegiatan Lurah')
+    this.$store.commit('setActivePage', 'Kegiatan Posyandu')
     this.$store.commit('setHeader', {
       navigations: [],
       descriptionPage: '',
